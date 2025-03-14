9b1aa538bdcb9d5ffef3a890f1643b8aab3f8c5e
zineflix <zineflix0@gmail.com>
2025-03-14 11:39:51 +0800

    Update firebase-config.js

--- Netplex/firebase-config.js
+++ Netplex/firebase-config.js
@@ -1,11 +1,9 @@
-async function getFirebaseConfig() {
-    const response = await fetch("https://netplex.streamhdmovie1.workers.dev");
-    return response.json();
-}
-
-getFirebaseConfig().then(config => {
-    import("https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js").then(({ initializeApp }) => {
-        const app = initializeApp(config);
-        console.log("Firebase initialized securely!");
-    });
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyB3hd4278XIZ86TAgR9UetC1Qa5M8KswlQ",
+    authDomain: "mynetplex-9fbbf.firebaseapp.com",
+    projectId: "mynetplex-9fbbf",
+    storageBucket: "mynetplex-9fbbf.firebasestorage.app",
+    messagingSenderId: "1028305253119",
+    appId: "1:1028305253119:web:ee07ea2678b046271152ca"
+};
+export { firebaseConfig };
