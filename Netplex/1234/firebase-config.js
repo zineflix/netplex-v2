f4d2bf9f1f8e2d68be60b29f7423c2a40db4ebd0
zineflix <zineflix0@gmail.com>
2025-03-14 10:23:49 +0800

    Delete Netplex/1234/firebase-config.js

--- Netplex/1234/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "mynetplex-9fbbf.firebaseapp.com",
-    projectId: "mynetplex-9fbbf",
-    storageBucket: "mynetplex-9fbbf.firebasestorage.app",
-    messagingSenderId: "1028305253119",
-    appId: "1:1028305253119:web:ee07ea2678b046271152ca"
-};
-export { firebaseConfig };
