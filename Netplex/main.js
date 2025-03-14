8ec666551db7c0a089f78ed35256267f295578b1
zineflix <zineflix0@gmail.com>
2025-03-14 09:59:29 +0800

    Update main.js

--- Netplex/main.js
+++ Netplex/main.js
@@ -3,16 +3,6 @@
     import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
     import { getFirestore, collection, addDoc, getDocs, query, orderBy, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
-    // Firebase configuration (Replace with your Firebase project details)
-    const firebaseConfig = {
-      apiKey: window.env.FIREBASE_API_KEY,
-      authDomain: window.env.FIREBASE_AUTH_DOMAIN,
-      projectId: window.env.FIREBASE_PROJECT_ID,
-      storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: window.env.FIREBASE_APP_ID,
-    };
-
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
