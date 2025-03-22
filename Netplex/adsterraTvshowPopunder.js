31f68c421171f8909cc2b76a342518d4375ea7a5
zineflix <zineflix0@gmail.com>
2025-03-22 22:28:33 +0800

    Update adsterraTvshowPopunder.js

--- Netplex/adsterraTvshowPopunder.js
+++ Netplex/adsterraTvshowPopunder.js
@@ -15,18 +15,65 @@ function triggerPopunder() {
 
     localStorage.setItem("popunderData", JSON.stringify({ ...savedData, [tvShowId]: today }));
 
-    openPopunder("https://acceptguide.com/w6gnwauzb?key=4d8f595f0136eea4d9e6431d88f478b5");
+    openPopupContainer("https://beddingfetched.com/w6gnwauzb?key=4d8f595f0136eea4d9e6431d88f478b5");
 }
 
-function openPopunder(url) {
-    let popunder = window.open(url, "_blank", "width=1,height=1,left=0,top=0");
-    if (popunder) {
-        popunder.blur();
-        window.focus();
-    }
+function openPopupContainer(url) {
+    const popup = document.createElement("div");
+    popup.style.position = "fixed";
+    popup.style.top = 0;
+    popup.style.left = 0;
+    popup.style.width = "100%";
+    popup.style.height = "100%";
+    popup.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    popup.style.zIndex = 9999;
+    popup.style.display = "flex";
+    popup.style.flexDirection = "column";
+    popup.style.alignItems = "center";
+    popup.style.justifyContent = "flex-start";
+
+    const skipBtn = document.createElement("button");
+    skipBtn.innerText = "Skip Ad";
+    skipBtn.style.position = "absolute";
+    skipBtn.style.top = "20px";
+    skipBtn.style.right = "20px";
+    skipBtn.style.padding = "10px 20px";
+    skipBtn.style.fontSize = "18px";
+    skipBtn.style.display = "none";
+    skipBtn.style.cursor = "pointer";
+    skipBtn.style.zIndex = 10000;
+    skipBtn.onclick = () => document.body.removeChild(popup);
+
+    const countdown = document.createElement("div");
+    countdown.style.color = "#fff";
+    countdown.style.fontSize = "24px";
+    countdown.style.marginTop = "60px";
+
+    const iframe = document.createElement("iframe");
+    iframe.src = url;
+    iframe.style.width = "90%";
+    iframe.style.height = "80%";
+    iframe.style.border = "none";
+
+    popup.appendChild(skipBtn);
+    popup.appendChild(countdown);
+    popup.appendChild(iframe);
+    document.body.appendChild(popup);
+
+    let timer = 10; // 10-second countdown
+    countdown.innerText = `Loading ad... Please wait ${timer} seconds to Skip...`;
+    const interval = setInterval(() => {
+        timer--;
+        countdown.innerText = `Loading ad... Please wait ${timer} seconds to Skip...`;
+        if (timer <= 0) {
+            clearInterval(interval);
+            countdown.innerText = "You can skip now.";
+            skipBtn.style.display = "block";
+        }
+    }, 1000);
 }
 
 function getTvShowIdFromURL() {
     const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get("id"); // Extract 'id' parameter from the URL
+    return urlParams.get("id");
 }
